944e4e1a64bb5093de8f610da1b2bda3dff6d83d
Bao Nguyen SM <bxnguyen@deloitte.ca>
2024-11-12 13:55:31 +0700

    update: ADD type to sanity

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import {categoryType} from './categoryType'
 import {productType} from './productType'
 import {sizeOptionType} from './sizeOptionType'
 import {postType} from './postType'
+import {contactInfoType} from './contactInfoType'
 
-export const schemaTypes = [categoryType, productType, sizeOptionType, postType]
+export const schemaTypes = [categoryType, productType, sizeOptionType, postType, contactInfoType]
